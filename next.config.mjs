da41737298893b77edf371f48f6b12f0bff2fd0f
Uzairkazi695 <167708998+Uzairkazi695@users.noreply.github.com>
2024-09-14 00:43:44 +0530

    Eslinting during build off

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains:["https://cloud.appwrite.io"]
-    }
+  images: {
+    domains: ["https://cloud.appwrite.io"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
